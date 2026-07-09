8e99c8af510cbca96afbaf7a2bafa19fac9c6916
Recyclace <recyclace@gmail.com>
2026-07-09 10:20:27 +0200

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://efugddifeqyvhbrtefid.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmdWdkZGlmZXF5dmhicnRlZmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTk1MzksImV4cCI6MjA5ODM3NTUzOX0.8A93mgkt0uHQZCjC65tMNzeYrZRHvKKcQmMoG4XmNJI'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
